f42871b51de28df1c1555542eb10ac6fa34b5270
Jacky <jacky123789456@gmail.com>
2022-04-09 16:38:28 +0800

    登入頁面需要新增事件

--- js/all.js
+++ js/all.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     console.log('hi')
     $('#datepicker1').datepicker()
     $('#datepicker2').datepicker()
+    $('.login__btn__submit').click(function() {
+        console.log('click')
+        $('.login__errorMessage__word').addClass('d-none')
+    })
 })
