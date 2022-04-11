e06f17ca13d889d586ad159ae2f360e40a429e34
Jacky <jacky123789456@gmail.com>
2022-04-11 10:23:46 +0800

    新增hover

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log('hi')
-    $('#datepicker1').datepicker()
-    $('#datepicker2').datepicker()
+    $('#datepicker1').datepicker({ dateFormat: "yy-mm-dd" })
+    $('#datepicker2').datepicker({ dateFormat: "yy-mm-dd" })
     $('.login__btn__submit').click(function() {
         console.log('click')
         $('.login__errorMessage__word').addClass('d-none')
