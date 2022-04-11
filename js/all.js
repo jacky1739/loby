09f936dff782a322f57e6da3815d72f1b4ce443e
Jacky <jacky123789456@gmail.com>
2022-04-11 11:40:59 +0800

    新增loading gif

--- js/all.js
+++ js/all.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     console.log('hi')
     $('#datepicker1').datepicker({ dateFormat: "yy-mm-dd" })
+    $('#datepicker1').attr( 'placeholder' )
+
     $('#datepicker2').datepicker({ dateFormat: "yy-mm-dd" })
     $('.login__btn__submit').click(function() {
         console.log('click')
